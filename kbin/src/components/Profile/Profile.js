1e4c9185ac050437f323eaf9f59f08d0a2ad371e
Yuda <49346133+YudaLegend@users.noreply.github.com>
2024-05-29 19:49:29 +0200

    añadido un stub de comments, solo se visualiza no se puede hacer ningun tipo de accion

--- kbin/src/components/Profile/Profile.js
+++ kbin/src/components/Profile/Profile.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import { getUserInfo,getUserThreads,getUserBoosts,getUserComments} from '../../services/api'; // Adjust the import according to your file structure
+import { useLocation, useParams } from 'react-router-dom';
+import { getUserInfo, getUserThreads, getUserBoosts, getUserComments } from '../../services/api'; // Adjust the import according to your file structure
 import UserHeader from './UserHeader';
-//import FilterMenu  from './FilterMenu';
 import OptionsMenu from './OptionsMenu';
 import Menu from '../Threads/Menu';
 import Filters from '../Threads/Filters';
 import Thread from '../Threads/Thread';
+import Comments from './Comments';
 
 const user = {
   id: 1,
@@ -22,74 +21,60 @@ const UserProfile = () => {
   const [comments, setComments] = useState([]);
   const [boosts, setBoosts] = useState([]);
 
-  //const [activeOrder, setActiveOrder] = useState('');
-  //const [activeFilter, setActiveFilter] = useState('');
   const { userId } = useParams(); // Assuming you're using a route parameter for userId
   const [order, setOrder] = useState('points'); // points, created_at, num_comments
   const [filter, setFilter] = useState('all');  // all, links, threads
   const [error, setError] = useState(null); // Estado para manejar errores
 
-
-  const isThreadsPage = location.pathname.includes('threads') || location.pathname === `/profile/${userId}` || location.pathname === `/profile/${userId}/`;
-
+  const isThreadsPage = location.pathname.includes('threads') || location.pathname === `/profile/${userId}`;
   const isBoostsPage = location.pathname.includes('boosts');
+  const isCommentsPage = location.pathname.includes('comments');
 
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const profileUserInfo = await getUserInfo(userId)
-        //const profileThreads = await getUserThreads(userId,filter,order)
-        
-        //const profileData = await getProfile(userId);
-        // Desestructurar los datos recibidos para obtener las partes necesarias
-        //const {threads, comments_count, boosts } = profileData;
+        const profileUserInfo = await getUserInfo(userId);
         setProfileUser(profileUserInfo);
-        //setThreads(profileThreads);
-
       } catch (error) {
         console.error('Error fetching profile:', error);
       }
     };
 
     fetchProfile();
-  }, [filter,order,userId]);
+  }, [userId]);
 
-
-  // useCallback: evita potenciales bucles infinitos de renderizado
   const fetchThreads = useCallback(async () => {
     setError(null); // Resetea cualquier error previo
     try {
-      const profileBoosts = await getUserBoosts(userId)
-      const profileComments = await getUserComments(userId)
-      const profileThreads = await getUserThreads(userId,filter,order)
-      
+      const profileThreads = await getUserThreads(userId, filter, order);
+      const profileBoosts = await getUserBoosts(userId);
+      const profileComments = await getUserComments(userId);
+
       const threadsWithTime = profileThreads.map(thread => ({
         ...thread,
         time_since_creation: timeElapsed(thread.created_at),
         time_since_update: timeElapsed(thread.updated_at),
         is_edited: isEdited(thread.created_at, thread.updated_at),
       }));
-      setThreads(threadsWithTime); // Actualiza el estado de los threads con los datos obtenidos
+
+      setThreads(threadsWithTime);
       setBoosts(profileBoosts);
       setComments(profileComments);
     } catch (error) {
       setError('Failed to fetch threads');
     }
-  }, [filter,order,userId]);
+  }, [filter, order, userId]);
 
-  useEffect(() => { // Efecto secundario para obtener los threads
-    fetchThreads(); // Ejecuta la función para obtener los threads
-  }, [fetchThreads]); // Dependencias: vuelve a ejecutar el efecto si `order` o `filter` cambian
+  useEffect(() => {
+    fetchThreads();
+  }, [fetchThreads]);
 
   if (error) {
     return <div>{error}</div>;
   }
-  
+
   if (!profileUser) {
-    return <div>Loading..aaa.</div>;
-  }
-  if (!Object.keys(profileUser).length) {
-    return <div>Loading..</div>;
+    return <div>Loading...</div>;
   }
 
   const isActive = (value, type) => {
@@ -100,8 +85,7 @@ const UserProfile = () => {
       return 'active';
     }
     return '';
-  }
-
+  };
 
   return (
     <div className="theme--dark" data-controller="kbin notifications" data-turbo="false">
@@ -112,23 +96,20 @@ const UserProfile = () => {
               <UserHeader profileUser={profileUser} />
               <OptionsMenu profileUser={profileUser} threadsCount={threads.length} commentsCount={comments.length} boostsCount={boosts.length} />
               {isThreadsPage && (
-        <aside className="options options--top" id="options">
-          <Menu setOrder={setOrder} isActive={isActive} />
-          <Filters setFilter={setFilter} isActive={isActive} />
-        </aside>
-      )}
-      <div id="content">
-        {isThreadsPage && (
-          threads.map(thread => (
-            <Thread key={thread.id} thread={thread} user={user} reloadThreads={fetchThreads} />
-          ))
-        )}
-        {isBoostsPage && (
-          boosts.map(thread => (
-            <Thread key={thread.id} thread={thread} user={user} reloadThreads={fetchThreads} />
-          ))
-        )}
-      </div>
+                <aside className="options options--top" id="options">
+                  <Menu setOrder={setOrder} isActive={isActive} />
+                  <Filters setFilter={setFilter} isActive={isActive} />
+                </aside>
+              )}
+              <div id="content">
+                {isThreadsPage && threads.map(thread => (
+                  <Thread key={thread.id} thread={thread} user={user} reloadThreads={fetchThreads} />
+                ))}
+                {isBoostsPage && boosts.map(thread => (
+                  <Thread key={thread.id} thread={thread} user={user} reloadThreads={fetchThreads} />
+                ))}
+                {isCommentsPage && <Comments comments={comments} user={user} />}
+              </div>
             </main>
           </div>
         </div>
